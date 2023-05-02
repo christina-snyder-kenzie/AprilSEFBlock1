56c127f32ed4be3e4f083b36768887c9c85107f5
christina-snyder-kenzie <christina.snyder@kenzie.academy>
2023-05-02 16:27:25 -0400

    Data Types

---
+++ Variables/src/main/java/org/example/DataTypeTalk.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class DataTypeTalk {
+    public static void main(String[] args) {
+         int x = 7;
+         x = 8;
+         x = 10;
+         //x = "hello";
+
+        double y = x;
+        y = y + 5;
+
+
+    }
+}
